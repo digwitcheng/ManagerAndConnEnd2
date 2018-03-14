036a55e6977b6c80e3ce27583b5a4c8cbd57d40a
digwitcheng <1046541763@qq.com>
2018-03-14 19:51:39 +0800

    20180314Dstar

---
+++ Algorithm/Dstar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agv.PathPlanning;
+using AGV_V1._0.Agv;
+
+namespace AGV_V1._0.Algorithm
+{
+    class Dstar : IAlgorithm
+    {
+        public int Search(Close[,] close, Node[,] graph, int beginX, int beginY, Direction beginDir)
+        {
+            return 0;
+        }
+    }
+}
